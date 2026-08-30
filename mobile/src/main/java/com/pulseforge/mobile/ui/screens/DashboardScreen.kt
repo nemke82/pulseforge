e81a402d2b63c140bb974f8cbb88724c42d8e645
nemke82 <ne@nemanja.io>
2026-08-30 20:17:55 +0200

    fix(mobile): restore Text import in DashboardScreen

--- mobile/src/main/java/com/pulseforge/mobile/ui/screens/DashboardScreen.kt
+++ mobile/src/main/java/com/pulseforge/mobile/ui/screens/DashboardScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
